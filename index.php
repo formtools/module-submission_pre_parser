7de58a0e377152c98791a962077cf0e490535c6b
Ben Keen <ben.keen@gmail.com>
2014-05-17 15:59:54 -0700

    1.1.2

--- index.php
+++ index.php
@@ -6,17 +6,18 @@ ft_init_module_page();
 $folder = dirname(__FILE__);
 require_once("$folder/library.php");
 
+$settings = ft_get_module_settings();
+$num_rules_per_page = $settings["num_rules_per_page"];
+
 if (isset($_POST["add_rule"]))
   list($g_success, $g_message) = spp_add_rule($_POST);
 else if (isset($_GET["delete"]))
   list($g_success, $g_message) = spp_delete_rule($_GET["delete"]);
 
 $page = ft_load_module_field("submission_pre_parser", "page", "page", 1);
-$rule_info = ssp_get_rules(10, $page);
+$rule_info = ssp_get_rules($num_rules_per_page, $page);
 $results     = $rule_info["results"];
 $num_results = $rule_info["num_results"];
-
-$settings = ft_get_module_settings();
 
 // ------------------------------------------------------------------------------------------------
 
@@ -24,7 +25,7 @@ $page_vars = array();
 $page_vars["head_title"]  = $L["module_name"];
 $page_vars["results"]     = $results;
 $page_vars["num_results"] = $num_results;
-$page_vars["pagination"] = ft_get_page_nav($num_results, $settings["num_rules_per_page"], $page);
+$page_vars["pagination"] = ft_get_page_nav($num_results, $num_rules_per_page, $page);
 $page_vars["js_messages"] = array("word_edit");
 $page_vars["head_js"] =<<< EOF
 var page_ns = {};
